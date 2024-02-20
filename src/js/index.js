c00f7934405cfc8ee913e90bdb6213d0e7449793
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-02-20 08:34:11 +0330

    [CSS]-Change Slider Btns Style

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,43 @@
-let mobileMenuBtn = document.querySelector('.mobile-menu-btn');
-let mobileMenu = document.querySelector('.mobile-menu');
-let flag = false
+let mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+let mobileMenu = document.querySelector(".mobile-menu");
+let flag = false;
 
 //* Logic For Show And Hide Mobile Menu
-mobileMenuBtn.addEventListener('click',()=>{
-    if(!flag){
-        mobileMenu.style.display = 'block'
-        flag =true;
-    }else{
-        mobileMenu.style.display = 'none'
-        flag =false;
-    }
+mobileMenuBtn.addEventListener("click", () => {
+  if (!flag) {
+    mobileMenu.style.display = "block";
+    flag = true;
+  } else {
+    mobileMenu.style.display = "none";
+    flag = false;
+  }
 });
 
-
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        items:2.2,
-        loop:true,
-        center:true,
-        autoWidth:true,
-        // nav:true,
-        // slideBy:1,
-        autoplay:true,
-        mergeFit:true,
-        responsive:{
-            0:{
-                items:1,
-                // nav:true
-            },
-            600:{
-                items:3,
-                // nav:false
-            },
-            1000:{
-                items:2,
-                // nav:true,
-                loop:false
-            }
-        }
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 2.2,
+    loop: true,
+    center: true,
+    autoWidth: true,
+    // nav:true,
+    // slideBy:1,
+    autoplay: true,
+    mergeFit: true,
+    responsive: {
+      0: {
+        items: 1,
+        // nav:true
+      },
+      600: {
+        items: 3,
+        // nav:false
+      },
+      1920: {
+        items: 2,
+        nav:false,
+        loop: true,
+      },
+    },
+    nav: true,
   });
-
+});
