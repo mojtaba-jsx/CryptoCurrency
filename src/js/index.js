aaf02d9cb264562c9654b6270045d6ff29687254
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-02-24 09:06:12 +0330

    [REPOSNIVE]- Add Reponsive For Story Section

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,4 @@
-let mobileMenuBtn = document.querySelector(".mobile-menu-btn");
-let mobileMenu = document.querySelector(".mobile-menu");
-let flag = false;
 
-//* Logic For Show And Hide Mobile Menu
-mobileMenuBtn.addEventListener("click", () => {
-  if (!flag) {
-    mobileMenu.style.display = "block";
-    flag = true;
-  } else {
-    mobileMenu.style.display = "none";
-    flag = false;
-  }
-});
 
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
