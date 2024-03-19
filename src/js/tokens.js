86653f01999c53cdf3f1c83b03f1f1dc15f3a6e6
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-03-19 09:19:01 +0330

    [REFACTOR]- Refactor NavBar Menu

---
+++ src/js/tokens.js
@@ -0,0 +1,5 @@
+// fetch(
+//   "https://api.coingecko.com/api/v3/coins/categories/list?x_cg_demo_api_key=CG-3bjv7fwArQ5Tw29Kii5swyL1"
+// )
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
