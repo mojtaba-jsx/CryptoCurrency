d64d1258d0e423c1af51fa1379d395abb2a3f716
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-04-01 09:14:35 +0330

    [Library]- Add Aos Library To Pages

--- src/js/shared.js
+++ src/js/shared.js
@@ -13,29 +13,34 @@ mobileMenuBtn.addEventListener("click", () => {
   }
 });
 
-
 //* Logic For SerchBox
-let navbarSearchBox = document.querySelector('.navbar__left-search')
-let navbarSearchIcon = document.querySelector('.navbar__middle-search-icon')
+let navbarSearchBox = document.querySelector(".navbar__left-search");
+let navbarSearchIcon = document.querySelector(".navbar__middle-search-icon");
 
-navbarSearchIcon.addEventListener('click',()=>{
-  let searchBoxValue =(navbarSearchBox.value).toLowerCase()
+navbarSearchIcon.addEventListener("click", () => {
+  let searchBoxValue = navbarSearchBox.value.toLowerCase();
 
   const options = {
     method: "GET",
     headers: { "x-cg-demo-api-key": "CG-3bjv7fwArQ5Tw29Kii5swyL1" },
   };
 
-  fetch(`https://api.coingecko.com/api/v3/coins/${searchBoxValue}`, options)
-    .then((response) => {
-      if(response.status ===200){
-        sessionStorage.setItem('CoinID',searchBoxValue)
-        location.href='./token-info.html'
-      }else{
-        location.href='./404.html'
+  fetch(
+    `https://api.coingecko.com/api/v3/coins/${searchBoxValue}`,
+    options
+  ).then((response) => {
+    if (response.status === 200) {
+      sessionStorage.setItem("CoinID", searchBoxValue);
+      location.href = "./token-info.html";
+    } else {
+      location.href = "./404.html";
+    }
+  });
+});
 
-      }
-    })
 
-  
-})
+AOS.init({
+  debounceDelay: 50, 
+  // throttleDelay:800, 
+  duration: 1000,
+});
